366ff94d937cbf98bee5b1f741a14a9b5eeafe49
James Dunne <james.jdunne@gmail.com>
2016-04-07 20:59:57 -0500

    Renamed xml files; updated master fader label.

--- osccommander/mkosc/main.go
+++ osccommander/mkosc/main.go
@@ -7,46 +7,6 @@ import (
 	"os"
 )
 
-type ts struct {
-	name  string
-	color string
-	track int
-}
-
-var track_setup = []ts{
-	{"Master", "red", 1},
-	{"Vox MG", "yellow", 2},
-	{"Vox JD", "yellow", 3},
-	{"Vox AS", "yellow", 4},
-	{"Gtr MG", "purple", 6},
-	{"Gtr JD", "purple", 7},
-	{"Bass", "purple", 8},
-	{"Kick", "green", 10},
-	{"Snare", "green", 11},
-	{"Overheads", "green", 12},
-}
-
-var colors = Colors{
-	ForeColor:   -50116,
-	BackColor:   -14803426,
-	TextColor:   -1,
-	BorderColor: -12566464,
-	ForeAlpha:   255,
-	BackAlpha:   255,
-	TextAlpha:   255,
-	BorderAlpha: 255,
-}
-var colors2 = Colors{
-	ForeColor:   16727100,
-	BackColor:   1973790,
-	TextColor:   -1775042766,
-	BorderColor: 16711422,
-	ForeAlpha:   0,
-	BackAlpha:   0,
-	TextAlpha:   150,
-	BorderAlpha: 0,
-}
-
 var colors_for = map[string]Colors{
 	"red": Colors{
 		ForeColor:   -50116,
@@ -100,12 +60,31 @@ var colors_for = map[string]Colors{
 	},
 }
 
+type ts struct {
+	name  string
+	color string
+	track int
+}
+
+var track_setup = [10]ts{
+	{"Master", "red", 1},
+	{"Vox MG", "yellow", 2},
+	{"Vox JD", "yellow", 3},
+	{"Vox AS", "yellow", 4},
+	{"Gtr MG", "purple", 6},
+	{"Gtr JD", "purple", 7},
+	{"Bass", "purple", 8},
+	{"Kick", "green", 10},
+	{"Snare", "green", 11},
+	{"Overheads", "green", 12},
+}
+
 func createLayout(bank int, layoutname string, mixname string, masterlabel string) {
 	var layout Layout
 
 	// Create layout:
 	layout.Version = layout_version
-	layout.LayoutName = layoutname + " - One In Ten"
+	layout.LayoutName = fmt.Sprintf("%d - %s", bank, layoutname)
 	layout.LayoutSummary = layoutname + " mix"
 	layout.Width = 1280
 	layout.Height = 752
@@ -127,6 +106,9 @@ func createLayout(bank int, layoutname string, mixname string, masterlabel strin
 
 	bank_track := (bank - 1) * 12
 	const spacing = 128
+
+	// Set master label:
+	track_setup[0].name = masterlabel
 
 	for t, ts := range track_setup {
 		track := ts.track + bank_track
@@ -184,26 +166,26 @@ func createLayout(bank int, layoutname string, mixname string, masterlabel strin
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%s\n", string(b))
+	//fmt.Printf("%s\n", string(b))
 
-	func() {
-		of, err := os.OpenFile(layoutname+".oc.xml", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
-		if err != nil {
-			panic(err)
-		}
-		// Write XML document:
-		of.Write(b)
-		err = of.Close()
-		if err != nil {
-			panic(err)
-		}
-	}()
+	// Create XML file:
+	of, err := os.OpenFile(fmt.Sprintf("OneInTen - %d - %s.oc.xml", bank, layoutname), os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	// Write XML document:
+	of.Write(b)
+	err = of.Close()
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
-	createLayout(1, "PA", "PA System", "Master")
-	createLayout(2, "JD", "Monitor for JD", "Master")
-	createLayout(3, "MG", "Monitor for MG", "Master")
-	createLayout(4, "MB", "Monitor for MB", "Master")
-	createLayout(5, "AS", "Monitor for AS", "Master")
+	createLayout(1, "PA", "PA System", "PA Master")
+	createLayout(2, "JD", "Monitor for JD", "JD Master")
+	createLayout(3, "MG", "Monitor for MG", "MG Master")
+	createLayout(4, "MB", "Monitor for MB", "MB Master")
+	createLayout(5, "AS", "Monitor for AS", "AS Master")
 }
